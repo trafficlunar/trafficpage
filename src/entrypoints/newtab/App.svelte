581ea04ec407048bf7b6e14a37adc0e575f278e2
trafficlunar <hello@trafficlunar.net>
2025-02-28 20:15:14 +0000

    feat: clock

--- src/entrypoints/newtab/App.svelte
+++ src/entrypoints/newtab/App.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+	import Clock from "@/lib/Clock.svelte";
 	import SearchBar from "@/lib/SearchBar.svelte";
 </script>
+
+<Clock />
 
 <main class="max-w-xl w-screen px-4">
 	<SearchBar />
---
+++ src/lib/Clock.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  let dateValue = $state("00:00 am");
+  let clockValue = $state("00:00 am");
+
+  setInterval(() => {
+    const date = new Date();
+
+    dateValue = date.toLocaleDateString("en-UK", { day: "numeric", month: "short", year: "numeric" });
+    clockValue = date.toLocaleTimeString("en-US").toLowerCase();
+  }, 10);
+</script>
+
+<div class="absolute top-4 left-4">
+  <p class="font-medium">{dateValue}</p>
+  <p class="text-overlay">{clockValue}</p>
+</div>
