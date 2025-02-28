62bc2cfb419c99dc692997a7ff1207705b55771b
trafficlunar <hello@trafficlunar.net>
2025-02-28 21:08:24 +0000

    feat: icon bookmarks

--- src/entrypoints/newtab/App.svelte
+++ src/entrypoints/newtab/App.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import Clock from "@/lib/Clock.svelte";
+	import IconBookmarks from "@/lib/IconBookmarks.svelte";
 	import SearchBar from "@/lib/SearchBar.svelte";
 </script>
 
 <Clock />
 
 <main class="max-w-xl w-screen px-4">
+  <IconBookmarks />
 	<SearchBar />
 </main>
