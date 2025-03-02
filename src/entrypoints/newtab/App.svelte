21a4e7164d9134a7ffd7066e5e3f396572b7f565
trafficlunar <hello@trafficlunar.net>
2025-03-02 21:11:58 +0000

    feat: bookmarks

--- src/entrypoints/newtab/App.svelte
+++ src/entrypoints/newtab/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Clock from "@/lib/Clock.svelte";
+	import Bookmarks from "@/lib/Bookmarks.svelte";
 	import IconBookmarks from "@/lib/IconBookmarks.svelte";
 	import SearchBar from "@/lib/SearchBar.svelte";
 	import Settings from "@/lib/Settings.svelte";
@@ -8,8 +9,9 @@
 <Clock />
 
 <main class="max-w-xl w-screen px-4">
-  <IconBookmarks />
+	<IconBookmarks />
 	<SearchBar />
+	<Bookmarks />
 </main>
 
 <Settings />
---
+++ src/lib/Bookmarks.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import Icon from "@iconify/svelte";
+
+	const data = [
+		[
+			{ name: "youtube", url: "https://youtube.com", icon: "iconoir:youtube-solid" },
+			{ name: "reddit", url: "https://reddit.com", icon: "fa6-brands:reddit" },
+			{ name: "bluesky", url: "https://bluesky.app", icon: "ri:bluesky-fill" },
+		],
+		[
+			{ name: "twitter", url: "https://twitter.com", icon: "mdi:twitter" },
+			{ name: "twitch", url: "https://twitch.tv", icon: "mdi:twitch" },
+			{ name: "github", url: "https://github.com", icon: "mdi:github" },
+			{ name: "last.fm", url: "https://last.fm", icon: "fa6-brands:lastfm" },
+		],
+		[
+			{ name: "twitter", url: "https://twitter.com", icon: "mdi:twitter" },
+			{ name: "twitch", url: "https://twitch.tv", icon: "mdi:twitch" },
+			{ name: "github", url: "https://github.com", icon: "mdi:github" },
+			{ name: "last.fm", url: "https://last.fm", icon: "fa6-brands:lastfm" },
+		],
+	];
+</script>
+
+<div class="grid justify-between mt-2" style="grid-template-columns: repeat({data.length}, minmax(0, auto));">
+	{#each data as category}
+		<div class="flex flex-col gap-0.5">
+			{#each category as bookmark}
+				<a href={bookmark.url} class="flex items-center gap-1.5 w-min px-1.5 py-0.5 rounded transition-colors hover:bg-accent/15 hover:text-accent">
+					<Icon icon={bookmark.icon} width="16" height="16" />
+					{bookmark.name}
+				</a>
+			{/each}
+		</div>
+	{/each}
+</div>
