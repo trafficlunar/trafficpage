93da58d64eb6a41f2897a5ce18ddf710928151c5
trafficlunar <hello@trafficlunar.net>
2025-03-02 21:12:07 +0000

    refactor: formatting

--- src/lib/IconBookmarks.svelte
+++ src/lib/IconBookmarks.svelte
@@ -1,19 +1,21 @@
 <script lang="ts">
 	import Icon from "@iconify/svelte";
 
-  const data = [
-    { name: "youtube", url: "https://youtube.com", icon: "iconoir:youtube-solid" },
-    { name: "reddit", url: "https://reddit.com", icon: "fa6-brands:reddit" },
-    { name: "bluesky", url: "https://bluesky.app", icon: "ri:bluesky-fill" },
-    { name: "twitter", url: "https://twitter.com", icon: "mdi:twitter" },
-    { name: "twitch", url: "https://twitch.tv", icon: "mdi:twitch" },
-    { name: "github", url: "https://github.com", icon: "mdi:github" },
-    { name: "last.fm", url: "https://last.fm", icon: "fa6-brands:lastfm" }
-  ]
+	const data = [
+		{ name: "youtube", url: "https://youtube.com", icon: "iconoir:youtube-solid" },
+		{ name: "reddit", url: "https://reddit.com", icon: "fa6-brands:reddit" },
+		{ name: "bluesky", url: "https://bluesky.app", icon: "ri:bluesky-fill" },
+		{ name: "twitter", url: "https://twitter.com", icon: "mdi:twitter" },
+		{ name: "twitch", url: "https://twitch.tv", icon: "mdi:twitch" },
+		{ name: "github", url: "https://github.com", icon: "mdi:github" },
+		{ name: "last.fm", url: "https://last.fm", icon: "fa6-brands:lastfm" },
+	];
 </script>
 
-<div class="flex justify-between py-4">
-  {#each data as bookmark}
-    <a href={bookmark.url} data-tooltip={bookmark.name}><Icon icon={bookmark.icon} width="32" height="32" class="transition duration-200 hover:text-accent" /></a>
-  {/each}
+<div class="flex justify-between mb-4">
+	{#each data as bookmark}
+		<a href={bookmark.url} data-tooltip={bookmark.name}>
+			<Icon icon={bookmark.icon} width="32" height="32" class="transition duration-200 hover:text-accent" />
+		</a>
+	{/each}
 </div>
--- src/lib/SearchBar.svelte
+++ src/lib/SearchBar.svelte
@@ -1,22 +1,29 @@
 <script lang="ts">
-	import Icon from '@iconify/svelte';
+	import Icon from "@iconify/svelte";
 
-  let searchValue = $state("");
+	let searchValue = $state("");
 
-  function submitSearch() {
-    window.location.href = `https://www.google.com/search?q=${searchValue}`;
-  }
+	function submitSearch() {
+		window.location.href = `https://www.google.com/search?q=${searchValue}`;
+	}
 
-  function searchOnEnter(e: KeyboardEvent) {
-    if (e.key == "Enter") submitSearch();
-  }
+	function searchOnEnter(e: KeyboardEvent) {
+		if (e.key == "Enter") submitSearch();
+	}
 </script>
 
-<div class="ring-accent/50 focus-within:border-accent rounded-lg border-2 focus-within:ring-3 transition duration-200">
+<div class="ring-accent/50 focus-within:border-accent rounded-lg border-2 focus-within:ring-3 transition duration-200 hover:bg-mantle/50">
 	<button onclick={submitSearch} class="absolute flex size-12 items-center justify-center">
 		<Icon icon="lets-icons:search" width="24" height="24" />
 	</button>
 
 	<!-- svelte-ignore a11y_autofocus -->
-	<input type="text" placeholder="Search" onkeydown={searchOnEnter} bind:value={searchValue} class="w-full p-3 ps-12 outline-none placeholder:text-overlay" autofocus />
+	<input
+		type="text"
+		placeholder="search"
+		onkeydown={searchOnEnter}
+		bind:value={searchValue}
+		class="w-full p-3 ps-12 outline-none placeholder:text-overlay"
+		autofocus
+	/>
 </div>
